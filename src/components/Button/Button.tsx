a3870dc02651170f7dd3faf550c3053fb13986a2
unknown <bilikouv07@gmail.com>
2022-02-05 16:27:45 +0100

    modify styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react"
 
 interface ButtonProps {
-  label: string
+	label: string
 }
 
-const Button = ({label}: ButtonProps) =>  {
-  return <button>{label}</button>;
+const Button = ({ label }: ButtonProps) => {
+	return (
+		<button
+			style={{
+				color: "cyan",
+				backgroundColor: "#222",
+				padding: "10px 24px",
+				borderRadius: 16,
+				outlineColor: "none",
+				outline: "none",
+			}}
+		>
+			{label}
+		</button>
+	)
 }
-
 
 export default Button
